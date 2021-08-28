c91c693d0fb23971dd4ecc6e4c50bebf74243f23
Gova7 <87258106+Gova7@users.noreply.github.com>
2021-08-29 00:48:06 +0530

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,28 @@
+function switchOff() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "switchOff";
+    document.getElementById("switch-On").style.backgroundColor = "#cbd2d9";
+    document.getElementById("switch-Off").style.backgroundColor = "#e12d39";
+
+}
+
+function switchOn() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "switchOn";
+    document.getElementById("switch-On").style.backgroundColor = "#22c55e";
+    document.getElementById("switch-Off").style.backgroundColor = "#cbd2d9";
+
+
+
+
+
+
+
+
+
+
+
+
+}
